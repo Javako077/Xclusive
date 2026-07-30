d321b5d80f72e70838425467af7de1ea3907510b
Javako077 <jyotipandeycselit@gmail.com>
2026-07-31 00:51:22 +0530

    update

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/xclusive");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`[MongoDB Connected] Host: ${conn.connection.host}`);
   } catch (error) {
     console.error(`[MongoDB Connection Error] ${error.message}`);
